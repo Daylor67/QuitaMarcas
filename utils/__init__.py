f883e09387979a458359d83e44d844c95a716f79
Daylor67 <felixpereira.fv@gmail.com>
2025-10-16 21:51:07 -0300

    Logica de watermakremove

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+"""
+utils - Utilidades generales para SmartStitch
+
+Este módulo contiene herramientas de utilidad para manejar archivos JSON
+y otras operaciones comunes.
+"""
+
+from .json_utils import UtilJson
+
+__version__ = '1.0.0'
+__all__ = ['UtilJson']
